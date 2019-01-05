248ac2c638a144df77ecf7e0507347cf963c1c9e
masumndc1 <masumndc1@gmail.com>
2019-01-05 23:43:25 +0200

    la 5.1.2019 23.43.25 +0200

---
+++ coding/go/test.go
@@ -0,0 +1,18 @@
+
+// this is a comment string in here
+/* this is another type of comment
+like as c/c++ style */
+
+package main
+
+import "fmt" 
+
+func main() {
+length := len("Hello World!")
+name := "mohammad khabir uddin"
+fmt.Println()
+fmt.Println("Hello World!")
+fmt.Println("1 + 1 =", 1 + 1)
+fmt.Println("Hello World is",length,"letters long")
+fmt.Println("my name is",len(name),"letters long")
+}
