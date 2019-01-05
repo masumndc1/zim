4659008d4f96f852ee1c9b52fb494c6501293b5c
masumndc1 <masumndc1@gmail.com>
2019-01-06 00:18:48 +0200

    su 6.1.2019 00.18.48 +0200

--- coding/go/test.go
+++ coding/go/test.go
@@ -1,4 +1,3 @@
-
 // this is a comment string in here
 /* this is another type of comment
 like as c/c++ style */
@@ -7,12 +6,15 @@ package main
 
 import "fmt" 
 
-func main() {
 length := len("Hello World!")
-name := "mohammad khabir uddin"
+var name string = "mohammad khabir uddin"
+
+func main() {
 fmt.Println()
 fmt.Println("Hello World!")
 fmt.Println("1 + 1 =", 1 + 1)
 fmt.Println("Hello World is",length,"letters long")
 fmt.Println("my name is",len(name),"letters long")
+fmt.Println("the first letter of my name is","name"[1])
+fmt.Println("the first letter of my name is"[1])
 }
