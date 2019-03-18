d9bd636f77efa5bd75d5cef53aabd3e0824adca8
masumndc1 <masumndc1@gmail.com>
2019-03-18 14:35:38 +0200

    some change

--- unix/debian/vim/vimrc_bash.sh
+++ unix/debian/vim/vimrc_bash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-apt-get install vim
-apt-get install git
+apt-get install -y -qq vim git > /dev/null
 
 echo -e " installing the vim plug "
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -15,9 +14,7 @@ fi
 echo -e "download vimrc file from git hub and place"
 curl -L https://raw.githubusercontent.com/masumndc1/zim/master/unix/debian/vim/vimrc_mine.txt -o ~/.vimrc
 
-
 echo -e " now you can run vim, then :source % :PlugInstall to "
 echo -e " install missing plugins "
-
 echo -e "we can install a lot of plugins from https://vimawesome.com"
 
