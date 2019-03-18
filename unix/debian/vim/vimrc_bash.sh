4107c1f8c8579a0f6768db0263dd5fd290531d32
masumndc1 <masumndc1@gmail.com>
2019-03-18 13:15:12 +0200

    vimrc bash creation

---
+++ unix/debian/vim/vimrc_bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get install vim
+
+echo -e " installing the vim plug "
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e "backing up the vimrc file first"
+if [ -f ~/.vimrc ] ; then
+    mv ~/.vimrc ~/.vimrc.bk
+fi
+
+echo -e "download vimrc file from git hub and place"
+curl -L -o ~/.vimrc https://raw.githubusercontent.com/masumndc1/zim/master/unix/debian/vim/vimrc_mine.txt
+
+echo -e " now you can run vim, then :source % :PlugInstall to "
+echo -e " install missing plugins "
+
+echo -e "we can install a lot of plugins from https://vimawesome.com"
+
