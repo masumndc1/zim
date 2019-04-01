427e70a9739584dacb595d914811d1c32c6b894d
masumndc1 <masumndc1@gmail.com>
2019-04-01 21:52:29 +0300

    nothing

---
+++ coding/python/practice/testing.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+import os 
+import sys
+import subprocess
+
+#if os.path.isdir(sys.argv[1]):
+#    print(sys.argv[1], "is a directory")
+#    print sys.argv[1], "is a directory"
+
+#else:
+#    print("this is not a directory")
+
+if __name__ = "__main__":
+    subprocess.call(["ls","-la"])
+
