aca0d5554fda3627d48c6f2a02abcaae156d98a2
Mohammad Khabir Uddin <masumndc1@gmail.com>
2019-09-17 23:37:15 +0300

    javascript was long time plan

--- coding/javascript/program.js
+++ coding/javascript/program.js
@@ -1,1 +1,15 @@
 document.writeln('Hello, world!');
+document.writeln(typeof 'hello');
+var numb = 20;
+document.writeln(numb.hasOwnProperty('number'));
+if (typeof numb == 'number') {
+   document.writeln('number' + ':' + numb);
+};
+
+var add = function (a, b) {
+  return a + b;
+};
+
+var adding = add.function();
+
+document.writeln(adding(4,7));
