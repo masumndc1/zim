041ae3aa9ccd772e98ee4c201888b8fd456c7dce
Mohammad Khabir Uddin <masumndc1@gmail.com>
2019-12-24 00:57:27 +0200

    new gt.lua

--- gt.lua
+++ gt.lua
@@ -7,25 +7,32 @@
  http://lua-users.org/lists/lua-l/2010-08/msg00300.html
 --]]
 
-
 function long_string()
   print(string.rep("-", 70))
 end
 
 function git_operation(msg)
   long_string()
-  print("pulling down first")
+--[[
+I want to print the output in colored font.
+the whole color scheme is coming from colr.png of
+this folder. the general coler codes are below
+print('\27[31mred')
+print('\27[32mgreen')
+print('\27[33myellow')
+print('\27[34mblue')
+print('\27[35mpurple')
+print('\27[36mlightgreen')
+print('\27[37mwhite')
+--]]
+  print("\27[35m Pulling down first \27[37m")
+  os.execute ('git pull')
   os.execute ('git add .')
-  print("commiting with msg")
+  print("\27[32m Commiting with msg '" .. msg .. "' \27[37m")
   os.execute ("git commit -m '" .. msg .. "' ")
-  print("pushing now to upstream")
+  print("\27[34m Pushing now to upstream \27[37m")
   os.execute ('git push origin master')
   long_string()
 end
-
---[[ 
- string=arg[1]
- print(string)
---]]
 
 git_operation(arg[1])
