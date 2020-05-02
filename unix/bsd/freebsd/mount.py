98461fa912392ed78e8c6bb67f4c5c7b0287c7c5
mohammad khabir uddin <masumndc1@gmail.com>
2020-05-02 16:38:20 +0300

    some improvement in mount.py

--- unix/bsd/freebsd/mount.py
+++ unix/bsd/freebsd/mount.py
@@ -3,11 +3,24 @@
 # at some point to mount point
 
 import os
+import subprocess
+
+def __doc__():
+    """usages: sudo ./mount.py"""
 
 def mountpoint(device='/dev/da0s1', mount_point='/mnt/usbmount'):
-    print("Mounting a USB Drive")
-    cmd = "mount.exfat-fuse {} {}".format(device, mount_point)
-    os.system(cmd)
+    
+    if os.path.exists(device):
+        print("Mounting a USB Drive")
+        cmd = "mount.exfat-fuse {} {}".format(device, mount_point)
+        res = os.system(cmd)
+        
+        if res:
+           #__doc__()
+           print("You need to be root")
+
+    else:
+        print("USB drive is not inserted: Please insert USB")
 
 if __name__ == "__main__":
     mountpoint()
