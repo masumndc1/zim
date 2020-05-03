a8c1d5695f8e139f3638c4b67e12d1fa2967c8b1
mohammad khabir uddin <masumndc1@gmail.com>
2020-05-03 17:09:47 +0300

    doc string and coloring in mount.py in freebsd

--- unix/bsd/freebsd/mount.py
+++ unix/bsd/freebsd/mount.py
@@ -2,25 +2,31 @@
 # This is a python script which will mount a usb located
 # at some point to mount point
 
+"""
+USB drive is not inserted: 
+Please insert USB. 
+Then run following.
+
+sudo python ./mount.py
+"""
+
 import os
 import subprocess
+from blessings import Terminal
 
-def __doc__():
-    """usages: sudo ./mount.py"""
+# custom variable
+mnt_cmd = '/usr/local/sbin/mount.exfat-fuse'
 
 def mountpoint(device='/dev/da0s1', mount_point='/mnt/usbmount'):
-    
+
     if os.path.exists(device):
         print("Mounting a USB Drive")
-        cmd = "mount.exfat-fuse {} {}".format(device, mount_point)
-        res = os.system(cmd)
-        
-        if res:
-           #__doc__()
-           print("You need to be root")
+        cmd = "sudo {} {} {}".format(mnt_cmd, device, mount_point)
+        os.system(cmd)
 
     else:
-        print("USB drive is not inserted: Please insert USB")
+        t = Terminal()
+        print(t.blue + __doc__ + t.normal)
 
 if __name__ == "__main__":
     mountpoint()
