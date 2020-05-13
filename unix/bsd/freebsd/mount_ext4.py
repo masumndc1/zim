d01fb59f916bb23d12b004df9cefda569d3e60fc
masumndc1 <masumndc1@gmail.com>
2020-05-13 18:03:39 +0300

    Mounting ext4 partition in freebsd

---
+++ unix/bsd/freebsd/mount_ext4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# This is a python script which will mount a usb located
+# at some point to mount point
+
+"""
+sudo python ./mount_ext4.py /dev/ada0p2 /mnt/ext4
+"""
+
+import os
+import sys
+
+def mountpoint(device=None, mount_point= None):
+
+    if not len(sys.argv) > 1:
+        print(__doc__)
+        
+    else:
+        print("Mounting ext4 partition")
+        device = sys.argv[1] 
+        mount_point = sys.argv[2]
+        cmd = "sudo mount -t ext2fs -o rw {} {}".format(device,mount_point)
+        os.system(cmd)
+
+if __name__ == "__main__":
+    mountpoint()
