12170df8c7f7767bbf345ad6d469c9842606c8ce
masumndc1 <masumndc1@gmail.com>
2020-05-19 14:19:40 +0300

    some eye candy styling

--- gt.py
+++ gt.py
@@ -11,31 +11,28 @@ class GitOperation():
   global term
   term = Terminal()
 
-
   def __init__(self):
     if len(sys.argv) == 2: 
       self.msg = sys.argv[1]
       GitOperation.GitPullPush(self.msg)
 
     elif len(sys.argv) < 2: 
-      print(term.red + "You want to pull updates from upstreams. Pulling update" + term.normal)
-      print(term.red + "If you dont want to pull, then usage is:" + term.normal)
-      print(term.red + "python3 gt.py \"commit msg\"" + term.normal)
+      print(term.red + "%20s" % "You want to pull updates from upstreams. Pulling update" + term.normal)
+      print(term.red + "%20s" % "If you dont want to pull, then usage is:" + term.normal)
+      print(term.red + "%20s" % "python3 gt.py \"commit msg\"" + term.normal)
       os.system('git pull')
-
 
   @classmethod
   def GitPullPush(cls, msg):
     GitOperation.GitPrint('*')
-    print(term.red + "Pulling first")
+    print(term.red + "%20s" % "Pulling first")
     os.system('git pull')
     os.system ('git add .')
-    print(term.blue + "Commiting with msg: " + "'" + msg + "'" + term.cyan)
-    os.system('git commit -m "%s"' % msg) 
-    print(term.green + "Pushing now to upstream " + term.cyan)
+    print(term.blue + "%20s" % "Commiting with msg: " + "'" + msg + "'" + term.cyan)
+    os.system('git commit -m "%20s"' % msg) 
+    print(term.green + "%20s" % "Pushing now to upstream " + term.cyan)
     os.system('git push origin master')
     GitOperation.GitPrint('-')
-
 
   @classmethod
   def GitPrint(cls, sym):
