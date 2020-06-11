625701477000f552747e03b8d2320eef7e02b71b
Mohammad Khabir Uddin <masumndc1@gmail.com>
2020-06-11 21:19:39 +0300

    slight changes in odering in gt script

--- gt.py
+++ gt.py
@@ -20,16 +20,16 @@ class GitOperation():
       print(term.red + "%20s" % "You want to pull updates from upstreams. Pulling update" + term.normal)
       print(term.red + "%20s" % "If you dont want to pull, then usage is:" + term.normal)
       print(term.red + "%20s" % "python3 gt.py \"commit msg\"" + term.normal)
-      os.system('git pull')
+      os.system('git pull --rebase')
 
   @classmethod
   def GitPullPush(cls, msg):
     GitOperation.GitPrint('*')
-    print(term.red + "%20s" % "Pulling first")
-    os.system('git pull')
     os.system ('git add .')
     print(term.blue + "%20s" % "Commiting with msg: " + "'" + msg + "'" + term.cyan)
     os.system('git commit -m "%s"' % msg) 
+    print(term.red + "%s" % "Pulling before pushing")
+    os.system('git pull --rebase')
     print(term.green + "%20s" % "Pushing now to upstream " + term.cyan)
     os.system('git push origin master')
     GitOperation.GitPrint('-')
