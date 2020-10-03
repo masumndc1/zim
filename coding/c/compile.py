c4914d2d926382b309d623632e10e3b5d30b7e44
masumndc1 <masumndc1@gmail.com>
2020-10-03 23:04:30 +0300

    modification of compile.py

--- coding/c/compile.py
+++ coding/c/compile.py
@@ -5,12 +5,13 @@
 import os
 import sys
 
+
 if len(sys.argv) == 2:
   # stripping out extention from file
   # left will be only file name without extension
   filename = sys.argv[1].rstrip(".c")
-  
-  if not os.path.exists("filename"):
-    os.system('gcc "%s" -o "%s"' %(sys.argv[1],filename))
-
+  os.system('gcc "%s" -o "%s"' %(sys.argv[1],filename))
   os.system('./"%s"' %filename)
+
+  # finally remove the file
+  os.system('rm "%s"' %filename)
