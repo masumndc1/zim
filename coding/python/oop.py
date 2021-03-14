82e7c1160fd7ac908aa1c730bedce4236947e626
masumndc1 <masumndc1@gmail.com>
2021-03-14 11:46:39 +0200

    Python: oop

--- coding/python/oop.py
+++ coding/python/oop.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
 class shareddata:
-	def method(self):
-		spam=42
-		email=60
-		print spam+email
-
+   def method(self):
+      spam=42
+      email=60
+      print(spam+email)
 
