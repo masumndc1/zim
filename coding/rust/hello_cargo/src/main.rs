517cf5588a46565857f8d8fe096e66e1806ca737
masumndc1 <masumndc1@gmail.com>
2021-03-14 20:04:38 +0200

    rust: testing number

--- coding/rust/hello_cargo/src/main.rs
+++ coding/rust/hello_cargo/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
     println!("Hello, world!");
-    // this is nice. is it not
+    // this is nice. is not it?
     println!("Hello, masum!");
+
+    number(8);
+
+    // let y = five(6);
+    println!("The value of y is: {}", y = five(6));
+}
+
+fn number(x: i8) {
+    let guess: u32 = "42".parse().expect("Not a Number!");
+    println!("The guess is {}", guess);
+    println!("The guess is {}", x);
+}
+
+fn five(x: i32) -> i32 {
+    let x = x + 1;
+    x
 }
--- coding/rust/hello_cargo/target/debug/deps/hello_cargo.d
+++ coding/rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -1,5 +1,5 @@
-/Users/khuddin/projects/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+/Users/khuddin/Documents/bektigoto/zim/coding/rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
 
-/Users/khuddin/projects/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+/Users/khuddin/Documents/bektigoto/zim/coding/rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
 
 src/main.rs:
