0b191d28148832e28af65da21e7419950f6502d5
masumndc1 <masumndc1@gmail.com>
2021-08-11 14:38:35 +0300

    python: more oop

--- coding/python/oop.py
+++ coding/python/oop.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 class shareddata:
-   def method(self):
-      spam=42
-      email=60
-      print(spam+email)
+    """this is something new"""
+    def __init__(self, spam, email):
+        self.spam = spam
+        self.email = email
+
+    def __str__(self):
+        return "Return total of %s and %s" % (self.spam, self.email)
+
+    def __repr__(self):
+        return "Account %s, balance %s" % (self.spam, self.email)
+
+    def emails_and_spam(self):
+       return self.spam + self.email
+
+    def emails_only(self):
+       return self.email
+
+    @property
+    def spam_only(self):
+       return self.spam 
+
 
