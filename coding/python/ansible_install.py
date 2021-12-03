866597a271cd4c6914334be19af323b0ef98edb1
masumndc1 <masumndc1@gmail.com>
2021-12-03 16:22:34 +0200

    making some changes on ansible_install.py

--- coding/python/ansible_install.py
+++ coding/python/ansible_install.py
@@ -4,49 +4,87 @@
 Python must be installed before hand. 
 Run OS command to install python first.
 for Freebsd run (pkg install python36) first
-for Ubuntu16  run: sudo python3 ansible_install.py
-for Centos7   run: sudo ./ansible_install.py
-for Freebsd12 run: sudo ./ansible_install.py
+for Ubuntu16  run: sudo apt install python
+for Centos7   run: sudo rpm install python
+for Freebsd12 run: sudo pkg install python
 """
 
 import os
 import platform
 
-def ansible_install():
+ansible_package = "ansible"
+
+    # first determine ansible package, pre_command and distro.
+    if 'FreeBSD' in platform.system() or 'DragonFly' in platform.system():
+        ansible_package = "py37-ansible"
+        os_package_manager = "pkg"
+
+    elif 'OpenBSD' in platform.system():
+        os_package_manager = "pkg_add"
+
+    elif 'Linux' in platform.system():
+        if 'Ubuntu' in platform.dist():
+           os_package_manager = "apt"
+
+        elif 'centos' in platform.dist() or 'rockylinux' in platform.dist():
+           os_package_manager = "yum"
+
+    else:
+        raise("We only support Unix Family")
+
+command = command(distro, os_package_manager, ansible_package)
+
+def command(distro=None, os_package_manager, ansible_package):
+    
+    if distro == 'Ubuntu' or distro == 'debian':
+       command = [ "%s update" % os_package_manager,
+            "%s install -y software-properties-common" % os_package_manager,
+            "apt-add-repository --yes --update ppa:ansible/ansible",
+            "'%s install -y %s', % (os_package_manager, ansible_package) 
+           ]
+    
+    if distro == 'CentOS' or distro == 'Rockylinux':
+       command = [ "%s install -y epel-release" % os_package_manager,
+                          "%s -y update" % os_package_manager,
+                          "yum install -y %s" % ansible_package
+                 ]
+
+    return command
+
+
+    # determine pre ansible commands
+
 # Ubuntu 
     if 'Linux' in platform.system():
       if 'Ubuntu' in platform.dist():
-        ubuntu_commands = [ "apt update", 
-            "apt install -y software-properties-common", 
-            "apt-add-repository --yes --update ppa:ansible/ansible",
-            "apt install -y ansible"
-           ]
+          distro = 'Ubuntu'
 
-        for command in ubuntu_commands:
+
+    for command in ubuntu_commands:
           os.system(command)
 
 # CentOS
       elif 'centos' in platform.dist():
-        centos_commands = [ "yum install -y epel-release",
+          distro = "CentOS"
+          commands = [ "yum install -y epel-release",
                           "yum -y update",
                           "yum install -y ansible" 
                         ]
 
-        for command in centos_commands:
-          os.system(command)
 
 # FreeBSD
     elif 'FreeBSD' in platform.system():
+        distro = 'FreeBSD'
+        
       freebsd_commands = ["pkg update -f -q",
                          "pkg install -y py37-ansible"
                         ]
       
-      for command in freebsd_commands:
-         os.system(command)
       
 # DragonFly
     elif 'DragonFly' in platform.system():
-      dragonfly_commands = [ "pkg install -y py37-ansible"
+        distro = 'DragonFly'
+        command = [ "pkg install -y py37-ansible"
                          ]
       
       for command in dragonfly_commands:
@@ -60,8 +98,11 @@ def ansible_install():
       for command in openbsd_commands:
          os.system(command)
 
-    
-ansible_install()
+
+if "__name__" == "__main__":
+    def run_command(command):
+        for commands in command:
+          os.system(commands)
 
 print("Ansible installation done")
 
