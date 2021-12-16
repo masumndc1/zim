ec84b815232d48182eeb340ec890a9d88eba3223
masumndc1 <masumndc1@gmail.com>
2021-12-17 00:36:28 +0200

    some more on ptr

--- coding/c/ptr.c
+++ coding/c/ptr.c
@@ -4,13 +4,27 @@
 int 
 main() 
 {
-    int iVar = 77; 
+    int iVar = 77, *iPtr; 
+    printf("%d\n", iVar);
     //int *iPtr;
-    //iPtr = &iVar;
-    int *iPtr = &iVar;
+    //*iPtr = &iVar;
+    iPtr = &iVar;
+    *iPtr = 80;
+    
 
-    printf("Value of iPtr (i.e. the address of iVar): %p\n"
-            "Address of iPtr: %p\n", iPtr, &iPtr);
+    // printf("Value of iPtr (i.e. the address of iVar): %p\n"
+    //        "Address of iPtr: %p\n", iPtr, &iPtr);
+    printf("%d\n", iVar);
+    ++*iPtr;
+    printf("%d\n", *iPtr);
+    //printf("%p\n", **iPtr);
+
+    printf("what does double dereferencing means?\n");
+    char a = 'z';
+    char *b = &a;
+    char **c = &b;
+    printf("a = %c, b = %p, c = %c\n",a,b, **c);
+
     
     return EXIT_SUCCESS;
 }
