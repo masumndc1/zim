316acd513077888448578e8159dffba456e22fee
masumndc1 <masumndc1@gmail.com>
2022-01-06 22:48:24 +0200

    C: Assertion

--- coding/c/assertion.c
+++ coding/c/assertion.c
@@ -1,35 +1,23 @@
-/*
- * =====================================================================================
- *
- *       Filename:  assertion.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  01/06/2022 21:34:24
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 
+
+int do_status (void) {
+  return 1;
+}
+
 int 
 main (void)
 {
-    int status = 1;
+  int status = 1;
 
-      for (int i = 0; i <= 10; ++i) {
-            for (int j = 0; j <= i; ++j) {
-                    printf("%d ", j);
-            }
-          printf("\n");
-       assert (status == 1);
+  for (int i = 0; i <= 10; ++i) {
+      for (int j = 0; j <= i; ++j) {
+        printf("%d ", j);
       }
+     printf("\n");
+     assert (do_status());
+  }
 }
 
