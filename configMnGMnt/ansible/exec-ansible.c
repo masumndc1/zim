38ab2b507e6341c34334f4c9e7c50d125ac00ecf
mohammad khabir uddin <masumndc1@gmail.com>
2022-04-24 19:16:56 +0300

    "Ansible installation script in C"

--- configMnGMnt/ansible/exec-ansible.c
+++ configMnGMnt/ansible/exec-ansible.c
@@ -1,55 +1,20 @@
-#define _POSIX_SOURCE
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
-#include <string.h>
 
+int main(void)
+{
+   printf("Installing ansible...\n");
+   char *command = "yum update\n"
+	           "yum install -y ansible";
 
-int main (int argc, char *argv[]) {
-
-   char *command; 
-
-   if (argc <= 1) {
-      printf("%s suse|opensuse|ubuntu|debian|rockylinux|centos|fedora|freebsd|dragonfly\n", argv[0]);
-      return 1;
-   }
-
-
-   if (!strcmp("debian", argv[1])
-             || !strcmp("ubuntu", argv[1])) {
-         command = "apt update -y\n"
-                   "apt install -y ansible";
-   }
-
-   else if (!strcmp("freebsd", argv[1]) 
-             || !strcmp("dragonfly", argv[1])) {
-         command = "pkg update -y\n"
-                   "pkg install -y py38-ansible";
-   }
-
-   else if (!strcmp("centos", argv[1]) 
-             || !strcmp("rockylinux", argv[1])
-	     || !strcmp("fedora", argv[1])) {
-         command = "yum update -y\n"
-                   "yum install -y ansible";
-   }
-
-   else if (!strcmp("suse", argv[1]) 
-             || !strcmp("opensuse", argv[1])) {
-         command = "zypper update -y\n"
-                   "zypper install -y ansible";
-   }
-
-   else {
-          printf("we dont support this Unix/Linux OS");
-   }
-   
-   /* install ansible using execl */
-
-   if (execl("/bin/sh","sh","-c", command, (char*)NULL) == -1 ) {
+   /* install ansible using execl and error check it */
+   if ( execl("/bin/sh","sh","-c", command, 
+      (char*)NULL) == -1 )
+   {
       perror("Can't execute program");
       return 1;
    }
-
    return 0;
 }
