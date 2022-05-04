51552a2b478bef7c139835685c14bb9a4234737e
Khabir Uddin <khuddin@csc.fi>
2022-05-04 20:59:01 +0300

    keep script simple

--- gt.py
+++ gt.py
@@ -5,39 +5,29 @@
 
 import os
 import sys
-from blessings import Terminal
 
 class GitOperation():
-  global term
-  term = Terminal()
 
   def __init__(self):
-    if len(sys.argv) == 2: 
+    if len(sys.argv) == 2:
       self.msg = sys.argv[1]
       GitOperation.GitPullPush(self.msg)
 
-    elif len(sys.argv) < 2: 
-      print(term.red + "%20s" % "You want to pull updates from upstreams. Pulling update" + term.normal)
-      print(term.red + "%20s" % "If you dont want to pull, then usage is:" + term.normal)
-      print(term.red + "%20s" % "python3 gt.py \"commit msg\"" + term.normal)
+    elif len(sys.argv) < 2:
+      print( "%20s" % "You want to pull updates from upstreams. Pulling update")
+      print( "%20s" % "If you dont want to pull, then usage is:")
+      print( "%20s" % "python3 gt.py \"commit msg\"")
       os.system('git pull --rebase')
 
   @classmethod
   def GitPullPush(cls, msg):
-    GitOperation.GitPrint('*')
-    print(term.red + "%s" % "Pulling before pushing")
+    print("20%s" % "Pulling before pushing")
     os.system('git pull --rebase')
     os.system ('git add .')
-    print(term.blue + "%20s" % "Commiting with msg: " + "'" + msg + "'" + term.cyan)
-    os.system('git commit -m "%s"' % msg) 
-    print(term.green + "%20s" % "Pushing now to upstream " + term.cyan)
+    print( "%20s" % "Commiting with msg: " + "'" + msg + "'")
+    os.system('git commit -m "%s"' % msg)
+    print( "Pushing now to upstream ")
     os.system('git push origin master')
-    GitOperation.GitPrint('-')
-
-  @classmethod
-  def GitPrint(cls, sym):
-    cls.num = term.width
-    print(term.red + sym * cls.num + term.normal)
 
 
 GitOperation()
