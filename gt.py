d380bf8fc248367a7b02534cca55ba3eb7151c92
Khabir Uddin <khuddin@csc.fi>
2022-05-16 00:23:48 +0300

    some modifications gt.py script

--- gt.py
+++ gt.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
 # this is a simple python script to automate
-# git pull, commit and push
-# to github.
+# git pull, commit and push to github.
 
-import os
 import sys
+import subprocess
+
 
 class GitOperation():
   '''
   gt.py "commit msg" branch_name
   '''
 
-  def __init__(self):
-    if len(sys.argv) == 2:
-      self.msg = sys.argv[1]
-      GitOperation.GitPullPush(self.msg)
+  def __init__(self, msg, branch):
 
-    elif len(sys.argv) < 2:
-      print( "%20s" % "You want to pull updates from upstreams. Pulling update")
-      print( "%20s" % "If you dont want to pull, then usage is:")
-      print( "%20s" % "python3 gt.py \"commit msg\"")
-      os.system('git pull --rebase')
+     self.msg = sys.argv[1]
+     self.branch = sys.argv[2]
+     self._add_new_files()
 
-  @classmethod
-  def GitPullPush(cls, msg):
-    print("20%s" % "Pulling before pushing")
-    os.system('git pull --rebase')
-    os.system ('git add .')
-    print( "%20s" % "Commiting with msg: " + "'" + msg + "'")
-    os.system('git commit -m "%s"' % msg)
-    print( "Pushing now to upstream ")
-    os.system('git push origin master')
+  def _add_new_files(self):
+     subprocess.call("git add .",shell=True)
+     self._commit()
+
+  def _commit(self):
+     subprocess.call('git commit -m "%s"' % self.msg, shell=True)
+     self._push()
+
+  def _push(self):
+     subprocess.call('git push origin "%s"' % self.branch, shell=True)
 
 
-if '__name__' == '__main__':
-   if len(sys.argv) < 2:
-       print(__doc__)
-   GitOperation()
+if __name__ == "__main__":
+   if len(sys.argv) == 3:
+       GitOperation(sys.argv[1], sys.argv[2])
+   else:
+       print(GitOperation.__doc__)
