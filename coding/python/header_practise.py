3046d89a0b07d932259c9ee4942c7d6871e688e3
Khabir Uddin <masumndc1@gmail.com>
2022-08-01 02:01:14 +0300

    python urllib practise

--- coding/python/header_practise.py
+++ coding/python/header_practise.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
 
+"""
+python3 header_practise.py http://www.webpage.com
+"""
 
 import sys
 from urllib.request import Request, urlopen
 import warnings
 
+
 class ViewWebPage():
     """This class will connect and fetch web pages and display them"""
 
-    def __init__(self, url:str=None) -> None:
-        self._url = url if sys.argv[1] else warnings.warn("you must provide the url")
+    def __init__(self):
+        try:
+            self._url = sys.argv[1]
+        except IndexError:
+            print("you have not passed url")
 
-    def _fetch_url(self):
-        """fetch the url and display"""
-        header = Request.add_header('Accept-Language', 'sv')
-        response=Request(self._url)
-        req=response.add_header('Accept-Language', 'sv')
-        s=urlopen(req)
-        print(s.readlines())
-        #req=Request('http://www.debian.org')
-    
+    def _fetch_url(self) -> None:
+        """fetch content of the url and display.
+        for example the content of http://www.debian.org"""
+
+        try:
+           response=Request(self._url)
+           s=urlopen(response)
+           print(s.readlines())
+        except AttributeError:
+            print("url is missing")
+
     def main(self):
         self._fetch_url()
 
