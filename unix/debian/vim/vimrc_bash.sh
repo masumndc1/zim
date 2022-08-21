77001c8dd49ff257837d9702eed56595fa71ee40
Mohammad Khabir Uddin <masumndc1@gmail.com>
2022-08-21 23:50:13 +0300

    vimrc_bash bash file modification

--- unix/debian/vim/vimrc_bash.sh
+++ unix/debian/vim/vimrc_bash.sh
@@ -3,18 +3,18 @@
 echo -e " testing and installing the vim plug "
 if [ ! -f ~/.vim/autoload/plug.vim ] ; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-echo -e "backing up the vimrc file first"
+echo -e " backing up the vimrc file first "
 if [ -f ~/.vimrc ] ; then
     mv ~/.vimrc ~/.vimrc.bk
 fi
 
-echo -e "download vimrc file from git hub and place"
+echo -e " download vimrc file from github and place it "
 curl -L https://raw.githubusercontent.com/masumndc1/zim/master/unix/debian/vim/vimrc_mine.txt -o ~/.vimrc
 
 echo -e " now you can run vim, then :source % :PlugInstall to "
 echo -e " install missing plugins "
-echo -e "we can install a lot of plugins from https://vimawesome.com"
+echo -e " we can install a lot of plugins from https://vimawesome.com "
 
