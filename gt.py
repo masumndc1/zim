48d3af4b5605982d243f9e94eb40ffb6a0dca641
Khabir Uddin <khuddin@csc.fi>
2022-08-26 22:37:49 +0300

    gt.py changed logic

--- gt.py
+++ gt.py
@@ -7,33 +7,37 @@ import subprocess
 
 
 class GitOperation():
-  '''
-  gt.py "commit_msg" branch_name
-  '''
+    '''
+    gt.py "commit_msg" branch_name
+    '''
 
-  def __init__(self, msg, branch):
+    def __init__(self, msg, branch):
+        self.msg = msg
+        self.branch = branch
+        self._add_new_files()
 
-     self.msg = sys.argv[1]
-     self.branch = sys.argv[2]
-     self._add_new_files()
+    def _add_new_files(self):
+        retcode = subprocess.call("git add .", shell=True)
+        self._commit() if not retcode else sys.exit("could not add files")
 
-  def _add_new_files(self):
-     retcode=subprocess.call("git add .",shell=True)
-     self._commit() if not retcode else sys.exit("could not add files")
+    def _commit(self):
+        retcode = subprocess.call(
+            'git commit -m "%s"' % self.msg, shell=True)
+        print("-"*70)
+        self._push() if not retcode else sys.exit("could not commit")
 
-  def _commit(self):
-     retcode=subprocess.call('git commit -m "%s"' % self.msg, shell=True)
-     print("-"*70)
-     self._push() if not retcode else sys.exit("could not commit")
-
-  def _push(self):
-     retcode=subprocess.call('git push origin "%s"' % self.branch, shell=True)
-     print("-"*70)
-     print(f"Pushed to {self.branch} branch") if not retcode else sys.exit("could not push")
+    def _push(self):
+        retcode = subprocess.call(
+            'git push origin "%s"' % self.branch, shell=True)
+        print("-"*70)
+        print(f"Pushed to {self.branch} branch") if not retcode else sys.exit(
+            "could not push")
 
 
 if __name__ == "__main__":
-   if len(sys.argv) == 3:
-       GitOperation(sys.argv[1], sys.argv[2])
-   else:
-       print(GitOperation.__doc__)
+    if len(sys.argv) == 3:
+        msg = sys.argv[1]
+        branch = sys.argv[2]
+        GitOperation(msg, branch)
+else:
+    print(GitOperation.__doc__)
