f9f6cd52ad176a5af5091d29697f1ffdd9544972
Khabir Uddin <khuddin@csc.fi>
2022-09-04 23:02:53 +0300

    gt.py terminal side

--- gt.py
+++ gt.py
@@ -2,6 +2,7 @@
 # this is a simple python script to automate
 # git pull, commit and push to github.
 
+import shutil
 import sys
 import subprocess
 
@@ -16,22 +17,26 @@ class GitOperation():
         self.branch = branch
         self._add_new_files()
 
+    def _term_size(self):
+        c, _ = shutil.get_terminal_size()
+        return c
+
     def _add_new_files(self):
+        print("-" * self._term_size())
         retcode = subprocess.call("git add .", shell=True)
         self._commit() if not retcode else sys.exit("could not add files")
 
     def _commit(self):
         retcode = subprocess.call(
             'git commit -m "%s"' % self.msg, shell=True)
-        print("-"*70)
         self._push() if not retcode else sys.exit("could not commit")
 
     def _push(self):
         retcode = subprocess.call(
             'git push origin "%s"' % self.branch, shell=True)
-        print("-"*70)
         print(f"Pushed to {self.branch} branch") if not retcode else sys.exit(
             "could not push")
+        print("-" * self._term_size())
 
 
 if __name__ == "__main__":
@@ -39,5 +44,5 @@ if __name__ == "__main__":
         msg = sys.argv[1]
         branch = sys.argv[2]
         GitOperation(msg, branch)
-else:
-    print(GitOperation.__doc__)
+    else:
+        print(GitOperation.__doc__)
