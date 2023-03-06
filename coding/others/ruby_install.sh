b67cd3eed38d27d4e7f7aebfda3b9334126850c5
Khabir Uddin <khuddin@csc.fi>
2023-03-06 11:53:07 +0200

    Install ruby on mac, options 2

--- coding/others/ruby_install.sh
+++ coding/others/ruby_install.sh
@@ -2,6 +2,7 @@
 
 
 ... install ruby in mac
+... option 1:
 
 brew install rbenv ruby-build
 
@@ -29,3 +30,22 @@ rbenv rehash
 gem update --system
 
 ... 
+... option 2:
+... brew install ruby
+... 
+... the newest version of the ruby might not get activated
+... as in mac there is another version is already installed.
+... you need to do following to activated your new version.
+...
+
+echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.zshrc
+
+... For compilers to find ruby you may need to set:
+export LDFLAGS="-L/usr/local/opt/ruby/lib"
+export CPPFLAGS="-I/usr/local/opt/ruby/include"
+
+... For pkg-config to find ruby you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+
+... 
+
