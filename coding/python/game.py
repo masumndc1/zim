d71ea6d8c16afc9348626ceced5bbeca51b1e6b0
Khabir Uddin <khuddin@csc.fi>
2025-02-20 18:50:37 +0200

    debugging of python and performance

--- coding/python/game.py
+++ coding/python/game.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import random
+# from IPython import embed
+# import sys
 
 count = 1
 while count < 100:
-    a = random.randint(0,100)
-    b = random.randint(0,100)
+    a = random.randint(0, 100)
+    #    sys.breakpointhook()
+    #    embed()
+    b = random.randint(0, 100)
     if a > b:
-        #c = a - b
-        #print("{} - {} = {}".format(a,b,c))
-        print("{}  {} - {}".format(count,a,b))
+        # c = a - b
+        # print("{} - {} = {}".format(a,b,c))
+        print("{}  {} - {}".format(count, a, b))
         count = count + 1
