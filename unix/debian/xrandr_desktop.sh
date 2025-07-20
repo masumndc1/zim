f30ccd733b1e3d1fd83f7c4d3d4b070969befbdc
Khabir Uddin <khuddin@csc.fi>
2025-07-20 20:51:59 +0300

    normal rotate by xrandr

--- unix/debian/xrandr_desktop.sh
+++ unix/debian/xrandr_desktop.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 xrandr --output LVDS --auto --primary
-xrandr --output HDMI-0 --auto --right-of LVDS --rotate left
+xrandr --output HDMI-0 --auto --right-of LVDS --rotate normal
+#xrandr --output HDMI-0 --auto --right-of LVDS --rotate left
