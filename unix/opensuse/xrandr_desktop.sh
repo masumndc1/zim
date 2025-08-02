bfd260e72a702389ccc9d7e26a4228bfb095a74b
Khabir Uddin <khuddin@csc.fi>
2025-08-03 01:27:59 +0300

    control monitor position by xrandr

---
+++ unix/opensuse/xrandr_desktop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#xrandr --output LVDS1 --auto --primary
+#xrandr --output HDMI1 --auto --right-of LVDS1 --rotate left
+#xrandr --output HDMI1 --auto --right-of LVDS1 --rotate normal
+xrandr --output HDMI1 --auto --primary
