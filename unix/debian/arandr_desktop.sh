9b9cbd4d57b075aea7fef5800cdfbdea9e187281
Khabir Uddin <khuddin@csc.fi>
2025-08-05 12:07:03 +0300

    arandr for debian to arrange desktop

---
+++ unix/debian/arandr_desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --mode 1366x768 --pos 1200x509 --rotate normal --output VGA-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate left --output DP-1 --off
