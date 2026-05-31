81d9e3bb398b06f0b86c8ac4e76b3c3dabae9334
Khabir Uddin <khuddin@csc.fi>
2026-05-31 22:29:53 +0300

    how to make dict from a line

---
+++ coding/lua/dict_split.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+
+local str = "os=ubuntu"
+local spl = "="
+local t = {}
+
+local match = string.match(str, spl)
+-- print(match)
+
+if string.len(match) then
+	for k, v in string.gmatch(str, "(%w+)" .. spl .. "(%w+)") do
+		-- print(k, v)
+		t[k] = v
+	end
+end
+
+for i, _ in pairs(t) do
+	print(i, t[i])
+end
