a0dcacb97a83298113fd08b9916db5cb066ee489
Khabir Uddin <khuddin@csc.fi>
2026-06-08 22:02:57 +0300

    about go lang

---
+++ coding/go/simple.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Declare and initialize a greeting variable
+	// greeting := "Hello, Go Developer!"
+	var greeting = "Hello, Go Developer!"
+
+	// Print the message to the console
+	fmt.Println(greeting)
+
+	// Display the current time formatted neatly
+	//now := time.Now().Format("Monday, Jan 2, 2006")
+	var now = time.Now().Format("Monday, Jan 2, 2006")
+
+	fmt.Printf("Today is %s.\n", now)
+	/*
+	   time.FixedZone()
+	   time.ANSIC = "something"
+	*/
+}
